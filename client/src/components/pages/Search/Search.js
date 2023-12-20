3e0301e8559d4d7eca60066caf8be0ebfdfb8bcb
Tomasz Cichon <tomekcichon@tlen.pl>
2023-12-20 21:43:14 +0100

    modify serch subpage

--- client/src/components/pages/Search/Search.js
+++ client/src/components/pages/Search/Search.js
@@ -5,38 +5,43 @@ import { API_URL } from '../../../config';
 import AdsList from '../../features/AdsList/AdsList';
 
 const Search = () => {
-    const [ads, setAds] = useState(null);
-    const { searchPhrase } = useParams();
-    useEffect(() => {
-        const fetchSearchAds = async () => {
-          try {
-            if (searchPhrase) {
-              const response = await fetch(`${API_URL}/api/ads/search/${searchPhrase}`);
-              if (response.ok) {
-                const adData = await response.json();
-                setAds(adData);
-              } else {
-                console.error('Failed to fetch ad');
-              }
-            }
-          } catch (error) {
-            console.error('Error fetching ad:', error);
-          }
-        };
-      
-        fetchSearchAds();
-      }, []);
-      
-        if (!ads) {
-          return <div>Loading...</div>;
-        }
+  const [ads, setAds] = useState(null);
+  const { searchPhrase } = useParams();
 
-        if (ads === undefined || !ads.length) {
-            return <Alert color="info">Loading...</Alert>;
-          } else if (ads.length === 0) {
-            return <Alert color="info">No Ads</Alert>;
+  useEffect(() => {
+    const fetchSearchAds = async () => {
+      try {
+        if (searchPhrase) {
+          const response = await fetch(`${API_URL}/api/ads/search/${searchPhrase}`);
+          if (response.ok) {
+            const adData = await response.json();
+            setAds(adData);
           } else {
-            return <AdsList ads={ads} />;
+            console.error('Failed to fetch ad');
           }
-}
+        }
+      } catch (error) {
+        console.error('Error fetching ad:', error);
+      }
+    };
+  
+    fetchSearchAds();
+  }, []);
+
+  if (!ads) {
+    return <div>Loading...</div>;
+  }
+
+  if (ads === undefined || !ads.length) {
+    return <Alert color="info">No Ads</Alert>;
+  }
+
+  return (
+    <div>
+      <h2>Search results</h2>
+      <AdsList ads={ads} />
+    </div>
+  );
+};
+
 export default Search;
